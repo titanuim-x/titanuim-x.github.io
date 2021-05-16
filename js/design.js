501f5514f3adf219b7f08feba54a94f624221860
NoNameTitan <50516342+NoNameTitan@users.noreply.github.com>
2021-05-16 13:18:57 +0500

    use

--- js/design.js
+++ js/design.js
@@ -1,97 +1,33 @@
-//#region Types
-/** @typedef {( ) => void } alpha */
-/** @typedef { number } n */
-/** @typedef { (value: any, index: n) => (void | boolean) } iterable */
-//#endregion
+import { tools, DOM } from "../node_modules/nonametitan_toolkit/index.js"
 
-//#region Tools
-const is = {
-    empty: value => value == undefined || value == null,
-    array: Array.isArray,
-    notClass: value => value == globalThis || is.empty(value),
-    func: value => "function" == typeof value
+const { is } = tools
+const { styler } = DOM
+
+/**
+ * @param {string} name
+ * @param {string} [value]
+ */
+let attr = (name, value) => {
+  let x = document.createAttribute(name)
+  is.empty(value) ? void 0 : x.value = value
+  return x
 }
-//#endregion
-
-//#region Iterate
 /**
- * @param { any[] } arr
- * @param { iterable } fn
+ * @param {HTMLDivElement} targe
+ * @param {Object} param1
+ * @param {Object} param1.pos
+ * @param {number} param1.pos.x
+ * @param {number} param1.pos.y
+ * @param {Object} param1.size
+ * @param {number} param1.size.x
+ * @param {number} param1.size.y
  */
-function forEach(arr, fn) { for (let i = 0; i < arr.length; i++) if (fn(arr[i], i) == true) return }
-/**
- * @param {{ }} obj
- * @param { (item: any, name: string) => (void | true) } fn
- */
-function forIn(obj, fn) { for (const z in obj) if (Object.hasOwnProperty.call(obj, z)) if (fn(obj[z], z) == true) return }
-//#endregion
+function Block(target, { pos, size }) {
+  if (!(target instanceof Element))
+    return console.error("TypeError: target is not a HTMLElement")
+  if (!target.hasAttribute("block")) target.setAttributeNode(attr("block"))
+  styler.pro(target, { "--position": `${pos.y}/${pos.x}/${size.y + pos.y}/${size.x + pos.x}` })
+  return target
+}
 
-//#region DOM
-const $$ = document, $ = query => $$.querySelector(query);
-$.all = query => $$.querySelectorAll(query)
-$.id = id => $$.getElementById(id)
-$.div = () => $$.createElement("div");
-//#endregion
-
-//#region CSS
-/**
- * @param { HTMLElement } target
- * @param { string | string[] } css
- */
-const add = (target, css) => {
-    if (is.array(css)) target.classList.add(...css)
-    else target.classList.add(css)
-},
-    remove = (target, css) => {
-        if (is.array(css)) target.classList.remove(...css)
-        else target.classList.remove(css)
-    };
-
-/**
- * @param { HTMLElement } target
- * @param { string } css
- */
-const contains = (target, css) => target.classList.contains(css),
-    toggle = (target, css) => {
-        let p = [target, css]
-        contains(...p) ? remove(...p) : add(...p)
-    };
-/**
- * @param { HTMLElement } target
- * @param { CSSStyleDeclaration | { }} css 
- * @returns 
- */
-const styler = (target, css) => forIn(css, (x, z) => $.div().style[z] = x)
-styler.pro = (target, css) => forIn(css, (x, z) => $.div().style.setProperty(z, x));
-//#endregion
-
-
-((g) => {
-    let hasDone = target => {
-        let _ = target.getAttribute("done")
-        return (_ == true || _ == "true")
-    }
-
-    let attrs = {
-        container: (target, value) => {
-            add(target, "grid")
-            styler.pro(target, "--" + value)
-            target.setAttribute("done", true)
-        },
-        row: (target, value) => { },
-        col: (target, value) => { },
-        center: (target, value) => { },
-        left: (target, value) => { },
-        right: (target, value) => { },
-        align: (target, value) => { },
-        flex: (target, value) => { },
-        grid: (target, value) => { },
-        x: (target, value) => { },
-        y: (target, value) => { },
-
-    }
-    forIn(attrs, (x, z) => forEach($.all("[" + z + "]"), y => {
-        if (!hasDone(y)) x(y, y.getAttribute(z))
-    }))
-
-})(globalThis)
+export { attr, Block }
--- js/index.js
+++ js/index.js
@@ -1,39 +1,62 @@
-const HOST_URL = "https://api.github.com/users"
-const REPOS = "repos"
-const USERS = [
-  "nonametitan",
-  "noname-titan"
-]
-const BODY = $.id("body")
+import { tools, DOM } from "../node_modules/nonametitan_toolkit/index.js"
+import { attr, Block } from "./design.js"
 
-/**
- * @param  {...string } str
- */
-function path(...str) {
-  let z = "", y = "/"
-  forEach(str, (x, i) => {
-    if ("string" !== typeof x) throw new TypeError("Bad argument")
-    x = x.trim()
-    if (i > 0 && x.slice(0, 3) == "../") { z = z.slice(0, z.lastIndexOf("/")); x = s.slice(2) }
-    if (i > 0 && x[0] !== y) x = y + x
-    if (i < str.length && x.endsWith(y)) x = x.slice(0, x.length - 1)
-    z += x
-  })
-  return z
-}
+const { each, is, path, request } = tools
+const { search, device, Div, styler, add, remove } = DOM
+
+const HOST_URL = "https://api.github.com/users",
+  REPOS = "repos",
+  USERS = [
+    "nonametitan",
+    "noname-titan"
+  ],
+  $$ = document,
+  BODY = search.id("body");
+
 /**
  * @param { string[] } css
  * @param { HTMLElement | HTMLDivElement } child
  * @param { string | "div" } tag
  */
-function Div(css, child, tag) {
-  let x = $$.createElement(tag ? tag : "div")
+function Div_(css, child, tag) {
+  let x = Div()
   if (Array.isArray(css)) x.classList.add(...css)
   if ("string" == typeof child) x.innerHTML = child
   else if (child instanceof HTMLElement) x.appendChild(child)
   return x
 }
-forEach(USERS, user => fetch(path(HOST_URL, user, REPOS))
-  .then(x => x.json()).then(x => forEach(x, y => BODY.appendChild(
-    Div(y.has_pages ? ["card", "gold"] : ["card"], `<h1>${y.name}</h1><h2>${y.owner.login}</h2><a href="${y.html_url}">Open</a>`)
+/**
+ * @param {Object} user
+ * @param {number} user.id
+ * @param {string} user.name
+ * @param {string} user.full_name
+ * @param {string} user.url
+ * @param {string} user.language
+ * @param {boolean} user.has_pages
+ * @param {Object} user.owner
+ * @param {string} user.owner.login
+ * @param {string} user.owner.id
+ * @param {string} user.owner.avatar_url
+ * @param {string} user.owner.url
+ * @param {string} user.owner.html_url
+ * @param {string} user.owner.type
+ */
+function CardRepo(user) {
+  let x = Div()
+  add(x, "card", "gold")
+  x.innerHTML = `<h1>${user.name}</h1><h2>${user.owner.login}</h2><a href="${user.html_url}">Open</a>`
+  return x
+}
+
+let content = search.id("content")
+content.appendChild(Block(Div(),{pos:{x:5,y:2},size:{x:1,y:2}}))
+
+each(USERS, user => request(path(HOST_URL, user, REPOS), (status, data) => {
+  console.log(data)
+}))
+
+each(USERS, user => fetch(path(HOST_URL, user, REPOS))
+  .then(x => x.json())
+  .then(x => each(x, y => BODY.appendChild(
+    Div_(y.has_pages ? ["card", "gold"] : ["card"], `<h1>${y.name}</h1><h2>${y.owner.login}</h2><a href="${y.html_url}">Open</a>`)
   ))))
