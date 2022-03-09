2f25e13c0aabda2f2dd088d57ae5399d544f9036
Aset Telmanov <telmanov2002.at@gmail.com>
2022-03-09 16:11:18 +0500

    ok

--- about/index.js
+++ about/index.js
@@ -1,4 +1,1 @@
-import { XStudio, XBlank, XSection, XContent, XList, XText, XFooter } from "https://x-titan.github.io/xstudio/index.js"
 import animation from "/core/js/animation.js"
-
-const xstudio = new XStudio("#app")
