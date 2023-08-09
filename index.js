1b64eeff3ddb6584e9611ac52ed52f6f550d4833
Aset Telmanov <telmanov2002.at@gmail.com>
2023-08-09 23:34:02 +0500

    ok

--- index.js
+++ index.js
@@ -1,36 +1,76 @@
-import { is, each } from "https://x-titan.github.io/utils/index.js"
+import { is, each, range } from "https://x-titan.github.io/utils/index.js"
 import { List } from "https://x-titan.github.io/list/index.js"
-import { search, css, validHTML } from "https://x-titan.github.io/web-utils/index.js"
+import { search, css, validHTML, isHTML } from "https://x-titan.github.io/web-utils/index.js"
 
 const g = globalThis
 const d = document
 const body = d.body
 
-console.log("A")
+const loadT = 750
+const longUpdT = 7000
+const loadAnimT = 500
+const updT = 50
 
 const noneVisibleQuery = "noneVisible"
 const textQuery = "h1,h2,h3,h4,h5,h6,p,a,li,em,i,strong,img,[noneVisible]"
 const textAnimationBlocks = search.all("[text-animation]")
 
-const textList = []
-
-each(textAnimationBlocks, (block) => {
-  textList.push(...search.all(textQuery, block))
-})
-
-console.log(textList)
+const textArray = []
+const textList = new List()
 
 function isVisible(target) {
-  const { top, bottom, height } = validHTML(target).getBoundingClientRect()
+  if (!isHTML(target)) return
+  const { top, bottom, height } = target.getBoundingClientRect()
 
   return (top + height >= 0) && (height + innerHeight >= bottom)
 }
 
+function hideAll() {
+  each(textArray, (text) => {
+    css.add(text, noneVisibleQuery)
+  })
+  textList.clear()
+  textList.fromArray(textArray)
+  console.log(textList)
+}
 function onScrollAnimation() {
-  each(textList, (text, i) => {
-    if (isVisible(item, i)) {
+  textList.filter((text) => {
+    console.log(text)
+    if (isVisible(text)) {
       css.remove(image, noneVisibleQuery)
+      // return false
     }
-    textList.splice(i, 1)
+    return true
   })
 }
+
+// new Promise((res, rej) => {
+//   let startTime = Date.now()
+//   const update = () => {
+//     if (
+//       (d.readyState === "complete")
+//       && ((startTime + loadT) < Date.now())
+//     ) return res()
+
+//     if ((startTime + longUpdT) < Date.now()) {
+//       return rej(new Error("It took a long time to load the website"))
+//     }
+
+//     setTimeout(update, updT)
+//   }
+//   // d.addEventListener(
+//   //   "DOMContentLoaded",
+//   //   d.onreadystatechange = update
+//   // )
+//   update()
+// }).then(() => {
+//   each(textAnimationBlocks, (block) => {
+//     textArray.push(...search.all(textQuery, block))
+//   })
+//   hideAll()
+// }).then(() => {
+//   g.addEventListener("scroll", () => {
+//     onScrollAnimation()
+//   })
+//   onScrollAnimation()
+// })
