65f4dc250903b551a3e3070e5c35dca20f3348d8
Aset Telmanov <telmanov2002.at@gmail.com>
2024-03-16 19:29:45 +0500

    ok

---
+++ gallery/index.js
@@ -0,0 +1,3 @@
+import { is, each, range, validate } from "https://x-titan.github.io/utils/index.js"
+import { search, css, validHTML, isHTML, attr, } from "https://x-titan.github.io/web-utils/index.js"
+
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { is, each, range, validate } from "https://x-titan.github.io/utils/index.js"
+import { search, css, validHTML, isHTML, attr, } from "https://x-titan.github.io/web-utils/index.js"
 import { List } from "https://x-titan.github.io/list/index.js"
 import * as path from "https://x-titan.github.io/path/index.js"
-import { search, css, validHTML, isHTML, attr, } from "https://x-titan.github.io/web-utils/index.js"
 import { Footer, Header, HeadCSS, HeadScript } from "/components.js"
 
 const g = globalThis
